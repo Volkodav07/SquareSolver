494bbe4220aafdf3374afeaf0147c13cc2331268
Almir Nasyrov <leggetbruno@gmail.com>
2023-08-29 03:47:39 +0300

    New functions
    Added tests, infinite input, main was separated on files

---
+++ colors.h
@@ -0,0 +1,15 @@
+#ifndef COLORS_H_
+#define COLORS_H_
+
+#define COLOR_STD "\033[39m"
+#define COLOR_BLACK "\033[30m"
+#define COLOR_WHITE "\033[97m"
+#define COLOR_RED "\033[91m"
+#define COLOR_GREEN "\033[92m"
+#define COLOR_BLUE "\033[94m"
+#define COLOR_PURPLE "\033[95m"
+#define COLOR_YELLOW "\033[33m"
+
+#define COLOR_RESET "\033[0m"
+
+#endif
---
+++ equation_solver.h
@@ -0,0 +1,59 @@
+int Linear_solver (const double b, const double c, double *x1, int *nRoots) // solving LinearSolver (a = 0, b != 0, c = 0)
+{
+     if (Compare_with_zero(b) == 0)
+     {
+        if (Compare_with_zero(c) == 0)
+        {
+            *nRoots = INF_ROOTS;
+
+        }
+
+        else
+        {
+            *nRoots = ZERO_ROOTS;
+        }
+     }
+
+     else
+     {
+        *x1 = -c / b;
+        *nRoots = ONE_ROOT;
+     }
+
+     return 0;
+}
+
+int Square_solver (const double a, const double b, const double c, double *x1, double *x2, int *nRoots)
+{
+    double D = b*b - 4*a*c;
+
+    if (Compare_with_zero (D) == -1)
+    {
+        *nRoots = ZERO_ROOTS;
+    }
+
+    double sqrt_D = sqrt (D);
+
+    if (Compare_with_zero (D) == 0)
+    {
+        *x1 = -b / (2*a);
+        *nRoots = ONE_ROOT;
+    }
+
+    if (Compare_with_zero (D) == 1)
+    {
+        *x1 = (-b + sqrt_D) / (2*a);
+        *x2 = (-b - sqrt_D) / (2*a);
+        *nRoots = TWO_ROOTS;
+    }
+
+    return 0;
+}
+
+int Equation_solver (double a, double b, double c, double &x1, double &x2, int &nRoots)
+{
+    if (Compare_with_zero (a) == 0) {Linear_solver (b, c, &x1, &nRoots);}
+    else {Square_solver (a, b, c, &x1, &x2, &nRoots);}
+
+    return 0;
+}
